9304eaa70bddaa2ffaeae6a5f90d035bc9d02ad8
Bill Schaller <bill@zeroedin.com>
2015-04-22 12:49:26 -0400

    （╯°□°）╯︵┻━┻

---
+++ lib/flip.php
@@ -0,0 +1,3 @@
+<?php
+function （╯°□°）╯︵┻━┻(){throw new ┻━┻;}
+class ┻━┻ extends  Exception {public function __construct() {parent::__construct("Please respect tables! ┬─┬ノ(ಠ_ಠノ)");} public function __toString(){return "┬─┬";}}
---
+++ tests/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class Test extends PHPUnit_Framework_TestCase {
+    /**
+     * @expectedException ┻━┻
+     * @expectedExceptionMessage Please respect tables! ┬─┬ノ(ಠ_ಠノ)
+     */
+    public function testCorrectExceptionIsThrown()
+    {
+        （╯°□°）╯︵┻━┻();
+    }
+}
