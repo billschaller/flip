56c0811d28ea64342f658c4ae65d441bbe1e0ff3
Daniel Mason <daniel.mason@thefoundry.co.uk>
2016-02-04 14:49:48 +0000

    Added test for object orientated throw

--- tests/Test.php
+++ tests/Test.php
@@ -10,4 +10,13 @@ class Test extends PHPUnit_Framework_TestCase
     {
         （╯°□°）╯︵┻━┻();
     }
+
+    /**
+     * @expectedException ┻━┻
+     * @expectedExceptionMessage Please respect tables! ┬─┬ノ(ಠ_ಠノ)
+     */
+    public function testObjectOrientatedExceptionIsThrown()
+    {
+        ┻━┻::ノ（°□°ノ）();
+    }
 }
